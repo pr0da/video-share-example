b350df3b44174d39776666bd0253e1662877a6bf
David Prokaj <prokajdavid@gmail.com>
2021-01-01 18:23:40 +0100

    add sample videos

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Asset, useAssets } from 'expo-asset';
+import * as FileSystem from 'expo-file-system';
+import { StyleSheet, Text, View, Button, Share } from 'react-native';
 
 export default function App() {
+  const [videoAssets] = useAssets([
+    require('./assets/sample-video.mp4'),
+    require('./assets/sample-video.webm'),
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {videoAssets?.map((asset) => (
+        <Button
+          onPress={async () => {
+            if (asset?.localUri) {
+              const contentUrl = await FileSystem.getContentUriAsync(
+                asset.localUri
+              );
+              Share.share({
+                url: contentUrl,
+              });
+            }
+          }}
+          title={`Share ${asset.name}.${asset.type}.`}
+        />
+      ))}
     </View>
   );
 }
